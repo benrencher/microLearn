06e3e0203a4fe301e5aa8d2aad611b0af67df579
bdrencher <bdrencher@gmail.com>
2023-02-16 21:52:43 -0700

    adjusting styles for login page

--- src/components/userInputField/userInputField.js
+++ src/components/userInputField/userInputField.js
@@ -4,7 +4,7 @@ function UserInputField(props) {
   return (
     <div className="UserInputField">
       <h3 className="UserInputTitle">{props.title}</h3>
-      <input type="text" placeholder={props.placeholder}/>
+      <input type="text" placeholder={props.placeholder} className="UserInputFieldInput" />
     </div>
   );
 }
